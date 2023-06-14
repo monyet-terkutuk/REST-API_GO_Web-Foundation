70ad0138d0ae88ab40ea220482150177df6c4cf1
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-06-14 22:28:37 +0700

    selesai handler untuk get list transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,5 +1,42 @@
 package handler
 
+import (
+	"go_api_foundation/helper"
+	"go_api_foundation/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransaction(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List campaign transaction", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
+
 // ------- Handler -----------
 // parameter dari uri
 // tangkap parameter dan mapping ke struct input
